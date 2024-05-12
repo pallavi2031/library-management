77213655fb289cf21a66c5135da1303ad635b456
pallavi <guptapallavi460@gmail.com>
2024-05-13 02:43:41 +0530

    .

--- frontend/src/client/backend-api/book.js
+++ frontend/src/client/backend-api/book.js
@@ -1,30 +1,30 @@
 const BookApi = {
   getAllBooks: async () => {
-    const res = await fetch("/v1/book", { method: "GET" })
+    const res = await fetch(`${process.env.BACKEND_URL}/v1/book`, { method: `GET` })
     return res.json()
   },
   getBookByIsbn: async (bookIsbn) => {
-    const res = await fetch(`/v1/book/${bookIsbn}`, { method: "GET" })
+    const res = await fetch(`${process.env.BACKEND_URL}/v1/book/${bookIsbn}`, { method: `GET` })
     return res.json()
   },
   addBook: async (data) => {
-    const res = await fetch("/v1/book", {
-      method: "POST",
+    const res = await fetch(`${process.env.BACKEND_URL}/v1/book`, {
+      method: `POST`,
       body: JSON.stringify(data),
       headers: { "Content-Type": "application/json" },
     })
     return res.json()
   },
   patchBookByIsbn: async (bookIsbn, data) => {
-    const res = await fetch(`/v1/book/${bookIsbn}`, {
-      method: "PATCH",
+    const res = await fetch(`${process.env.BACKEND_URL}/v1/book/${bookIsbn}`, {
+      method: `PATCH`,
       body: JSON.stringify(data),
       headers: { "Content-Type": "application/json" },
     })
     return res.json()
   },
   deleteBook: async (bookIsbn) => {
-    const res = await fetch(`/v1/book/${bookIsbn}`, { method: "DELETE" })
+    const res = await fetch(`${process.env.BACKEND_URL}/v1/book/${bookIsbn}`, { method: `DELETE` })
     return res.json()
   },
 }
--- frontend/src/client/backend-api/user.js
+++ frontend/src/client/backend-api/user.js
@@ -1,38 +1,38 @@
 const UserApi = {
   borrowBook: async (isbn, userId) => {
-    const res = await fetch("/v1/user/borrow", {
-      method: "POST",
+    const res = await fetch(`${process.env.BACKEND_URL}/v1/user/borrow`, {
+      method: `POST`,
       body: JSON.stringify({ isbn, userId }),
       headers: { "Content-Type": "application/json" },
     })
     return res.json()
   },
   returnBook: async (isbn, userId) => {
-    const res = await fetch("/v1/user/return", {
-      method: "POST",
+    const res = await fetch(`${process.env.BACKEND_URL}/v1/user/return`, {
+      method: `POST`,
       body: JSON.stringify({ isbn, userId }),
       headers: { "Content-Type": "application/json" },
     })
     return res.json()
   },
   getBorrowBook: async () => {
-    const res = await fetch("/v1/user/borrowed-books", { method: "GET" })
+    const res = await fetch(`${process.env.BACKEND_URL}/v1/user/borrowed-books`, { method: `GET` })
     return res.json()
   },
   login: async (username, password) => {
-    const res = await fetch("/v1/user/login", {
-      method: "POST",
+    const res = await fetch(`${process.env.BACKEND_URL}/v1/user/login`, {
+      method: `POST`,
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
     })
     return res.json()
   },
   getProfile: async () => {
-    const res = await fetch("/v1/user/profile", { method: "GET" })
+    const res = await fetch(`${process.env.BACKEND_URL}/v1/user/profile`, { method: `GET` })
     return res.json()
   },
   logout: async () => {
-    const res = await fetch("/v1/user/logout", { method: "GET" })
+    const res = await fetch(`${process.env.BACKEND_URL}/v1/user/logout`, { method: `GET` })
     return res.json()
   },
 }
