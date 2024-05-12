87a85469ed24e56d114f3d19f1637ec5acb3b14d
pallavi <guptapallavi460@gmail.com>
2024-05-13 02:48:50 +0530

    cors 2

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,12 @@ const cors = require("cors")
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://library-management-frontend-zeta.vercel.app'
+  ]
+}))
 app.use(morgan("dev"))
 app.use(express.json())
 app.use(cookieParser())
